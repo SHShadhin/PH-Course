b7e36986de273f02bb09bbef1a2a24d1af32d2d0
SHShadhin <shshadhin0@gmail.com>
2026-04-10 14:41:09 +0600

    Recap data fetching and display data

---
+++ Milestone-7 React Router Dom/Module-41-Next.js-Rendering_Basics/my-second-next-app/src/app/foods/page.jsx
@@ -0,0 +1,24 @@
+import FoodCard from "@/components/FoodCard";
+
+const FoodsPage = async () => {
+  // const res = await fetch('');
+  // const data = await res.json();
+
+  const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
+  const data = await res.json();
+  const foods = data.data
+  console.log(foods)
+  return (
+    <div>
+      <h2>Foods: {foods.length} </h2>
+
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-6 px-5">
+        {
+          foods.map(food=> <FoodCard food={food} key={food.id}></FoodCard>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default FoodsPage;
